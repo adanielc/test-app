1e39312216630864931c52fd52011f8208e98d13
andresdaniel@gmail.com <danielcastillobermudez@MacBook-Pro-de-Daniel.local>
2024-03-20 19:20:38 +0100

    first commit

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-list>
+      <q-expansion-panel>
+        <q-card>
+          <q-card-section> {{ pepe }} </q-card-section>
+        </q-card>
+      </q-expansion-panel>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pepe: "texto de pepe",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
