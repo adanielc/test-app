1045fcf24c3dad64ec1bc82d1ff3544e1fa9073f
adanielc <andresdaniel@gmail.com>
2024-04-01 16:23:56 +0200

    second commit

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,10 +1,9 @@
 <template>
-  {{ answers }}
-  <q-page class="flex flex-center">
+  <q-page>
     <q-list>
       <q-expansion-item
         expand-separator
-        v-for="(question, key) in preguntas"
+        v-for="question in preguntas"
         :key="question.id"
         :label="question.pregunta"
       >
@@ -13,7 +12,19 @@
             <q-option-group
               :options="question.opciones"
               :true-value="question.opciones.value"
-              v-model="answers[question.opciones.value]"
+              :model-value="modelValue"
+              v-model="answersModel[question.id]"
+              @update:model-value="
+                (newStore) => {
+                  updateOptions(
+                    newStore,
+                    question.id,
+                    question.respuesta,
+                    question.tipo,
+                    question.pregunta
+                  );
+                }
+              "
             />
           </q-card-section>
         </q-card>
@@ -24,10 +35,16 @@
 </template>
 
 <script>
+import json from "../../public/questions1.json";
+
 export default {
   data() {
     return {
       answers: [],
+      answersModel: [],
+      modelValue: "",
+      vmo: "",
+      qu: json,
       preguntas: [
         {
           id: 1,
@@ -93,11 +110,74 @@ export default {
           tema: "",
         },
       ],
+      validacion: [
+        {
+          pregunta: 1,
+          respuesta: 1,
+        },
+        {
+          pregunta: 2,
+          respuesta: 2,
+        },
+        {
+          pregunta: 3,
+          respuesta: 3,
+        },
+      ],
     };
   },
   methods: {
     submit() {
-      alert("Submit");
+      let validResponses = this.answers.filter((ans) => ans.isCorrect == true);
+      console.log("validResponses: " + validResponses.length);
+      let invalidResponses = this.answers.filter(
+        (ans) => ans.isCorrect == false
+      );
+      let inv = invalidResponses.map((q) => {
+        q.tipo, q.pregunta;
+      });
+      console.log("nuevo mapa: " + inv);
+      console.log("invalidResponses: " + JSON.stringify(invalidResponses));
+      console.log("invalidResponses length: " + invalidResponses.length);
+      let subjects = Map.groupBy(invalidResponses, (grupo) => {
+        return grupo.tipo;
+      });
+      let sub = Array.from(subjects);
+      console.log("sub: " + sub);
+      console.log("sub json: " + JSON.stringify(sub));
+      let total = this.preguntas.length;
+      let grade = (validResponses.length / total) * 100;
+      console.log("total: " + total);
+      console.log("grade: " + grade);
+      console.log("subjects: " + subjects);
+      alert(grade);
+    },
+    updateOptions(value, key, response, tipo, pregunta) {
+      console.log("value: " + value);
+      console.log("key: " + key);
+      console.log("modelValue: " + this.modelValue);
+      let answer = {
+        quesion: key,
+        option: value,
+        pregunta: pregunta,
+        isCorrect: response == value ? true : false,
+        tipo: tipo,
+      };
+      if (this.answers != null && this.answers != undefined) {
+        console.log("answers not null");
+        let result = this.answers.find((obj) => {
+          console.log("obj: " + JSON.stringify(obj));
+          console.log("obj question: " + obj.quesion);
+          return obj.quesion === key;
+        });
+        console.log("result: " + JSON.stringify(result));
+        if (result != undefined) {
+          console.log("result not null");
+          this.answers.pop(result);
+        }
+      }
+      this.answers.push(answer);
+      console.log("answers: " + JSON.stringify(this.answers));
     },
   },
 };
